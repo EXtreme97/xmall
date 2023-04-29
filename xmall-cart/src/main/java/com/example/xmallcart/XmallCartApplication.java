fe93f60dbc7abcce5d87fd4c3745261910d270e4
xzy <xuzhenyuan30@gmail.com>
2023-04-29 23:05:33 +0800

    认证和单点登录

---
+++ xmall-cart/src/main/java/com/example/xmallcart/XmallCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmallcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallCartApplication.class, args);
+    }
+
+}
---
+++ xmall-cart/src/test/java/com/example/xmallcart/XmallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmallcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
